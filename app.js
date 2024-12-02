6c833d68c41ae6c533e488502cabac55e83cadb9
Rahul Jangid <136984487+RahulJangid7779@users.noreply.github.com>
2024-12-02 23:01:19 +0530

    learn about IMG and CI-ID

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const Header = () => {
       <div className="logo-container">
         <img
           class="logo "
-          src="https://imgs.search.brave.com/FZ5KiFSpo7sg8lRLNa9H4OvGEFFiYR3x6qrC7VfWkBg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/ZnJlZS12ZWN0b3Iv/Zm9vZC1sb2dvLWJh/Y2tncm91bmRfMTM5/MC03Mi5qcGc_c2Vt/dD1haXNfaHlicmlk"
+          src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_96,h_96/portal/c/logo_2022.png"
         />
       </div>
       <div className="nav-item">
@@ -35,27 +35,35 @@ const Header = () => {
   );
 };
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+  const { resData } = props;
   return (
     <div className="res-card">
       <img
         className="res-img"
         src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/5b4acd02d56ffe7c32f9b9d076018e4a"
       />
-      <h3>Meghana Food</h3>
-      <h6>Pizza,North Indian,Asian</h6>
-      <h6>4.4 stars</h6>
-      <h6>38 mins </h6>
+      <h3>{resData.data.name}</h3>
+      <h6>{resData.data.cusine}</h6>
+      <h6>{resData.data.rating}</h6>
+      <h6>{resData.data.time} </h6>
     </div>
   );
+};
+const resObj = {
+  data: {
+    name: "KFC",
+    cusine: "pizza, burger,mocktail",
+    rating: 2.2,
+    time: 35,
+  },
 };
 const Body = () => {
   return (
     <div className="body">
-      <div className="search"></div>
+      <div className="search">search</div>
       <div className="res-container">
-        <div className="    ">Search</div>
-        <RestaurantCard />
+        <RestaurantCard resData={resObj} />
       </div>
     </div>
   );
